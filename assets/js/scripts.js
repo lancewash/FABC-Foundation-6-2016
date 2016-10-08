2e161c6d0b8fadae47146c2fffa506a75957c568
lancewash <lancewashington@msn.com>
2016-10-08 17:02:07 -0400

    Beginnings of GSAPv2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,9 @@
 /**
  * Created by lance on 10/7/2016.
  */
+var logo = document.getElementById('logo');
+var banner = document.getElementById('head-banner');
 
-TweenMax.to('.logo', 1, {bottom:500})
+TweenLite.to(banner, 20, {opacity:1});
+TweenLite.to(logo, 2, {opacity:1});
+
