ff8e0f3c8407b426271594aa4befc221dda8817e
lancewash <lancewashington@msn.com>
2016-10-08 23:05:48 -0400

    gsap files added v1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,22 @@
  */
 var logo = document.getElementById('logo');
 var banner = document.getElementById('head-banner');
+var intro1 = document.getElementById('gsap-intro-text-1');
+var intro2 = document.getElementById('gsap-intro-text-2');
+var header = document.getElementById('header');
+var pastor = document.getElementById('pastor');
+var welcome = document.getElementById('welcome');
 
-TweenLite.to(banner, 20, {opacity:1});
-TweenLite.to(logo, 2, {opacity:1});
+$(document).ready(function(){
+    startGSAP();
+});
 
+function startGSAP(){
+    TweenMax.to(banner, 12, {delay:2,opacity:1});
+    TweenMax.to(logo, 1, {opacity:1});
+    TweenMax.to(intro1, 2, { opacity:1, ease:Power2.easeInOut });
+    TweenMax.to(intro2, 6, { opacity:1, ease:Power2.easeInOut });
+    TweenMax.to(header, 3, { delay:3, opacity:1, ease:Power0.easeInOut });
+    TweenMax.to(pastor, 3, { delay:3, opacity:1, ease:Power0.easeInOut });
+    TweenMax.to(welcome, 3, { delay:3, opacity:1, ease:Power0.easeInOut });
+}
