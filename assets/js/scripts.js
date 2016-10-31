cc661a91641598a8e2147ebceb025a1a6febd73d
lancewash <lancewashington@msn.com>
2016-10-31 05:05:00 -0400

    modified tween

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,26 +2,30 @@
  * Created by lance on 10/7/2016.
  */
 
+var intro1 = document.getElementById('gsap-intro-text-1');
+var intro2 = document.getElementById('gsap-intro-text-2');
 var header = document.getElementById('header');
 var banner = document.getElementById('head-banner');
 var logo = document.getElementById('logo');
-var intro1 = document.getElementById('gsap-intro-text-1');
-var intro2 = document.getElementById('gsap-intro-text-2');
-var pastor = document.getElementById('pastor');
-var welcome = document.getElementById('welcome');
+var pastorImg = document.getElementById('pastor-img');
+var pastorText = document.getElementById('pastor-txt');
+var welcome1 = document.getElementById('welcome-1');
+var welcome2 = document.getElementById('welcome-2');
 
 $(document).ready(function(){
     startGSAP();
 });
 
 function startGSAP(){
-   TweenMax.to(header, .5, { delay:4, opacity:1, ease:Power2.easeInOut });
-   TweenMax.to(banner, 2, {delay:4,opacity:1});
-   TweenMax.to(logo, 1, { opacity:1 });
-   TweenMax.to(intro1, 2, { opacity:1, ease:Power0.easeInOut });
-   TweenMax.to(intro2, 2, { delay:2, opacity:1, ease:Power0.easeInOut });
-   TweenMax.to(pastor, .75, { delay:4.5, opacity:1, ease:Power0.easeInOut });
-   TweenMax.to(welcome, 1, { delay:4, opacity:1, ease:Power0.easeInOut });
+   TweenMax.to(intro1, 1, { opacity:1, ease:Power0.easeInOut });
+   TweenMax.to(intro2, 2.5, { delay:1, opacity:1, ease:Power0.easeInOut });
+   TweenMax.to(header, 2, { delay:3, opacity:1 });
+   TweenMax.to(banner, 3, {delay:3,opacity:1 });
+   TweenMax.to(logo, 1.5, { delay:3, opacity:1 });
+   TweenMax.to(pastorImg, 2, { delay:3, opacity:1, ease:Power0.easeInOut });
+   TweenMax.to(pastorText, 2, { delay:2, opacity:1, ease:Power0.easeInOut });
+   TweenMax.to(welcome1, 3, { delay:2, opacity:1, ease:Power0.easeInOut });
+   TweenMax.to(welcome2, 2, { delay:3, opacity:1, ease:Power0.easeInOut });
 }
 
 // function startGSAP(){
